e302bfa0107135760a9ef581066046444e08b87c
Johnthan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-03-28 14:38:52 -0700

    added some animations and transitions

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,5 @@
 function About() {
-  return (
-    <>
-      <div className="w-screen aspect-[3/1] bg-white dark:bg-zinc-700"></div>
-      <h1>This is all about me!!!</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default About;
