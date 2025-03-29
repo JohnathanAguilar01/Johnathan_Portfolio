1ba3b957061df059638b3cbafdf10931ce47ff2a
Johnathan Aguilar <johnathan.aguilar.350@my.csun.edu>
2025-03-29 16:16:14 -0700

    fixed the about me section to look half decent

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,29 @@
 function About() {
   return (
     <>
-      <h2 className="text-3xl font-bold text-center mb-8 mt-4">About Me</h2>
-      <p className="text-lg md:text-xl leading-relaxed text-gray-700 dark:text-gray-300 max-w-4xl mx-auto mt-8 p-6">
-        I'm a senior at California State University, Northridge, where I've
-        built a strong foundation in computer science, from data structures and
-        algorithms to software engineering and operating systems. My academic
-        journey has been enriched by a minor in data science, equipping me with
-        essential skills in probability, statistical modeling, and machine
-        learning. Alongside my studies, I’ve explored practical applications
-        through personal projects in web development and database implementation
-        using technologies like React, Node.js, Express.js, MySQL, and AWS.
-      </p>
+      <div className="flex px-64 py-24 justify-between w-screen">
+        <div className="max-w-2xl h-auto flex flex-col items-start justify-center">
+          <h2 className="text-3xl font-bold text-start text-blue-300 mb-4">
+            About Me
+          </h2>
+          <p className="text-lg font-bold">
+            I'm a senior at California State University, Northridge, where I've
+            built a strong foundation in computer science, from data structures
+            and algorithms to software engineering and operating systems. My
+            academic journey has been enriched by a minor in data science,
+            equipping me with essential skills in probability, statistical
+            modeling, and machine learning. Alongside my studies, I’ve explored
+            practical applications through personal projects in web development
+            and database implementation using technologies like React, Node.js,
+            Express.js, MySQL, and AWS.
+          </p>
+        </div>
+        <img
+          className="size-125 rounded-2xl"
+          src="profile_pic.jpg"
+          alt="Profile Picture"
+        />
+      </div>
     </>
   );
 }
